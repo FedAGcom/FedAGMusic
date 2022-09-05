58b945a995e3c52297b49cf93f15847451631c5b
SoS <bazzaev98@gmail.com>
2022-09-05 21:09:30 +0300

    FM-8, добавил CRUD операции controller и service, + тесты + Slf4j

--- src/main/java/com/fedag/fedagmusic/service/impl/UserServiceImpl.java
+++ src/main/java/com/fedag/fedagmusic/service/impl/UserServiceImpl.java
@@ -4,6 +4,8 @@ import com.fedag.fedagmusic.entities.User;
 import com.fedag.fedagmusic.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -15,29 +17,35 @@ import java.time.LocalDateTime;
 @Slf4j
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    Logger logger = LoggerFactory.getLogger("Logger");
 
     private final UserRepository userRepository;
 
     @Override
     public Mono<User> getUserById(Long id) {
+        logger.info("Выполняется метод getUserById");
         return userRepository.findById(id);
     }
 
     @Override
     public Mono<User> addUser(User user) {
+        logger.info("Выполняется метод addUser");
         user.setCreated(LocalDateTime.now());
+        logger.info("Объект сохранен");
         return userRepository.save(user);
     }
 
     @Transactional
     @Override
     public Mono<Void> deleteUserById(Long id) {
+        logger.info("Выполняется метод deleteUserById");
         return userRepository.deleteById(id);
 
     }
 
     @Override
     public Mono<User> updateUser(User user, Long id) {
+        logger.info("Выполняется метод updateUser");
         return userRepository.findById(id).
                 map((c) -> {
                     c.setEmail(user.getEmail());
