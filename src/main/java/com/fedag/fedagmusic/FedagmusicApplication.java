8f3ed105d775ab9322ede434f28e3957aa6baf08
Anatolii Vasilev <avas2012@gmail.com>
2022-09-07 00:50:03 +0300

    added spring security webflux token JWT, BCryptPasswordEncoder

--- src/main/java/com/fedag/fedagmusic/FedagmusicApplication.java
+++ src/main/java/com/fedag/fedagmusic/FedagmusicApplication.java
@@ -2,12 +2,14 @@ package com.fedag.fedagmusic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class FedagmusicApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FedagmusicApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FedagmusicApplication.class, args);
+
+    }
 
 }
